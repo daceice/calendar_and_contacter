5714b9c25a3453d9786251139edb0089f17a8696
Yun Shao <daceice@gmail.com>
2011-10-28 12:14:41 +0800

    minor update

--- app/models/calendar.rb
+++ app/models/calendar.rb
@@ -3,7 +3,7 @@ class Calendar < ActiveRecord::Base
   
   def content
     if self.note
-      return self.note.gsub("\n",'<br>').gsub("\n\r",'<br>').gsub("\r\n",'<br>')
+      return self.note.gsub("\n\r",'<br>').gsub("\r\n",'<br>').gsub("\n",'<br>')
     else
       return ''
     end
